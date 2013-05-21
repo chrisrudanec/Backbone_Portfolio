50f7f837e09516fefa2f5cb7313cddab462ad0c7
Dan Garland <dan@dangarland.co.uk>
2013-05-21 14:37:45 -0400

    Got user pages going

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -1,5 +1,7 @@
 app.models.User = Backbone.Model.extend({
 
+  localStorage: new Backbone.LocalStorage('portfolio-user'),
+
   full_name: function() {
     return this.attributes.first_name + " " + this.attributes.last_name;
   }
--- app/assets/javascripts/views/project_view.js
+++ app/assets/javascripts/views/project_view.js
@@ -23,19 +23,10 @@ app.views.ProjectView = Backbone.View.extend({
     projectList.forEach(function(project) {
       var view = new app.views._Project({ model: project });
       _this.$el.find('#project-list').append(view.render().el);
-      //this.model.toJSON()
-    });
-
-    var me = new app.models.User({
-      first_name: "Corey",
-      last_name: "Trombley",
-      image_url: 'uploads/corey.jpg',
-      bio: 'Well ard coder from NYC',
-      mission: 'Complete level 12 on Bomberman and save the world from itself'
     });
 
     var bio = new app.views.UserView({
-      model: me
+      model: this.model
     }).render();
     this.$el.find('#user-bio').html(bio.el);
 
