3757fa723cefe172bcbd1bd4e08fdc3255659b10
Dan Garland <dan@dangarland.co.uk>
2013-05-21 13:54:23 -0400

    Refactored templates

--- app/assets/javascripts/views/user_view.js
+++ app/assets/javascripts/views/user_view.js
@@ -2,18 +2,12 @@ app.views.UserView = Backbone.View.extend({
 
   tagName: 'div',
   id: 'bio',
-  template: _.template($('#user-template').html()),
+  template: JST['templates/user'],
   render: function() {
     // Get some HTML from somewhere
-    var html = this.template({
-      user: this.model
-    });
-
+    var html = this.template({user: this.model });
     // Append it to this view's div#bio element
     this.$el.html(html);
-
-    // Attach it to the DOM
-    $('#user-bio').html(this.el);
 
     return this;
   }
