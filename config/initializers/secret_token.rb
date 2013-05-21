c1306b2654d9b85289b39718d3419793e3928b30
Dan Garland <dan@dangarland.co.uk>
2013-05-21 12:11:04 -0400

    Adding rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backbone::Application.config.secret_token = '57ea54c2071213f7378b2b833e6a82f7844ad9377f208a1ca9d1b957694531bc5dd8697ea148b5d83765bcd4cbb8710635a73dc28036f5414ee54cac455f50d6'
