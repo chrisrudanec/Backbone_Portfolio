b907f211d1bb42d04d47defbefa6b52ee3de774f
Dan Garland <dan@dangarland.co.uk>
2013-05-21 09:01:42 -0400

    Refactoring to use a router

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,41 +1,6 @@
 $(document).ready(function() {
-  // Try to find projects already in the local storage
-  var projectList = new app.collections.ProjectList();
-  projectList.fetch();
 
-  // Create a dummy project if there isn't one already
-  if(projectList.length == 0) {
-    var bucket_list = projectList.create({
-      title: "Bucketlist",
-      url: "https://github.com/dmgarland/BucketListApp",
-      body: "<p>I worked on a Rails application that created a todo list of things I want to do before I die.</p> <ul> <li>I integrated Google maps and used Geocoding to determine where my activities would take place.</li> <li>I used AJAX to asynchronously update markers on the map when the center changed.</li> <li>I displayed crime statistics on a chart using an API call and Morris.js</li> </ul>"
-    });
-  }
-
-  // Create a blank project for us to fill in.
-  projectList.add({
-    title: "New Project",
-    url: "Click to edit",
-    body: "Click to edit"
-  });
-
-  // projectList.forEach(function(project) {
-  //   var view = new app.views.ProjectView({ model: project });
-  //   $('#project-list').append(view.render().el);
-  // });
-
-  // Create a view for the first Project and render it
-  // var view = new app.views.ProjectView({ model: projectList.first() });
-  // $('#project-list').append(view.render().el);
-
-  var me = new app.models.User({
-    first_name: "Corey",
-    last_name: "Trombley",
-    image_url: 'uploads/corey.jpg',
-    bio: 'Well ard coder from NYC',
-    mission: 'Complete level 12 on Bomberman and save the world from itself'
-  });
-  new app.views.UserView({
-    model: me
-  }).render();
+  var router = new app.Router();
+  Backbone.history.start({ pushState: true});
+  router.navigate('');
 });
